e23ab24817d6a9c601c5db3877c924f9bc58eedb
EnderModZe <manuel.arquati@gmail.com>
2023-07-21 13:19:34 +0200

    easter egg

--- project/dist/login.js
+++ project/dist/login.js
@@ -4,6 +4,9 @@ function login() {
     let password = document.getElementById("password");
     console.log("ciao");
     if (email.value === "admin@admin" && password.value === "password") {
+        window.location.href = "../pages/admin.html";
+    }
+    else if (email.value === "user@user" && password.value === "password") {
         window.location.href = "../pages/home.html";
     }
     else {
--- project/src/login.ts
+++ project/src/login.ts
@@ -3,7 +3,9 @@ function login(){
     let email = document.getElementById("email") as HTMLInputElement;
     let password = document.getElementById("password") as HTMLInputElement;
     console.log("ciao")
-    if(email.value === "admin@admin" && password.value === "password"){
+    if (email.value === "admin@admin" && password.value === "password"){
+        window.location.href = "../pages/admin.html"
+    }else if (email.value === "user@user" && password.value === "password"){
         window.location.href = "../pages/home.html"
     }else{
         alert("EMAIL E/O PASSWORD ERRATI") 

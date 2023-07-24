ffc60c6145208395205380bf277e5eb734642bac
EnderModZe <manuel.arquati@gmail.com>
2023-07-24 09:49:28 +0200

    Ultime feature

--- project/dist/login.js
+++ project/dist/login.js
@@ -1,15 +1,44 @@
 "use strict";
+let credenzialiList = [
+    { email: "user@user",
+        password: "password" },
+    { email: "gino@panino",
+        password: "pasuord" }
+];
 function login() {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    console.log("ciao");
+    let isLoggedIn = false;
+    credenzialiList.forEach(x => {
+        if (email.value === x.email && password.value === x.password) {
+            window.location.href = "../pages/home.html";
+            isLoggedIn = true;
+        }
+    });
     if (email.value === "admin@admin" && password.value === "password") {
         window.location.href = "../pages/admin.html";
+        isLoggedIn = true;
     }
-    else if (email.value === "user@user" && password.value === "password") {
-        window.location.href = "../pages/home.html";
-    }
-    else {
+    if (!isLoggedIn) {
         alert("EMAIL E/O PASSWORD ERRATI");
+    }
+}
+function register() {
+    let newEmail = document.getElementById("email");
+    let newPassword = document.getElementById("password");
+    let isRegistered = false;
+    credenzialiList.forEach(x => {
+        if (newEmail.value === x.email) {
+            alert("Utente già registrato");
+            isRegistered = true;
+        }
+    });
+    if (!isRegistered) {
+        let newUser = {
+            email: newEmail.value,
+            password: newPassword.value
+        };
+        credenzialiList.push(newUser);
+        alert("Utente registrato");
     }
 }
--- project/src/login.ts
+++ project/src/login.ts
@@ -1,14 +1,55 @@
+type Credenziali = {
+    email: string,
+    password: string
+};
+ 
+let credenzialiList: Credenziali[] = [
+    {email: "user@user",
+    password: "password"},
+    {email: "gino@panino",
+    password: "pasuord"}
+];
+
 
 function login(){
     let email = document.getElementById("email") as HTMLInputElement;
     let password = document.getElementById("password") as HTMLInputElement;
-    console.log("ciao")
+    let isLoggedIn: boolean = false;
+    credenzialiList.forEach(x => {
+        if (email.value === x.email && password.value === x.password){
+            window.location.href = "../pages/home.html"
+            isLoggedIn = true;
+        }
+
+    })
     if (email.value === "admin@admin" && password.value === "password"){
         window.location.href = "../pages/admin.html"
-    }else if (email.value === "user@user" && password.value === "password"){
-        window.location.href = "../pages/home.html"
-    }else{
+        isLoggedIn = true;
+    }
+    if(!isLoggedIn){
         alert("EMAIL E/O PASSWORD ERRATI") 
     }
 }
+
+function register(){
+    let newEmail = document.getElementById("email") as HTMLInputElement;
+    let newPassword = document.getElementById("password") as HTMLInputElement;
+    let isRegistered:boolean = false;
+    credenzialiList.forEach(x => {
+        if (newEmail.value === x.email){
+            alert("Utente già registrato")
+            isRegistered = true;
+        }
+    })
+
+    if(!isRegistered){
+        let newUser:Credenziali = {
+            email:newEmail.value,
+            password: newPassword.value
+        }
+        credenzialiList.push(newUser);
+        alert("Utente registrato");
+    }
+}
+
 
